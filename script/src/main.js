61c12d60b3621d6379a455cdb91650b5595688ec
Жужликова Екатерина Алексеевна <katya.zhuzhlikova.02@mail.ru>
2022-04-08 02:05:36 +0300

    Попытка в слайдеры

---
+++ script/src/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 5,
+    spaceBetween: 15,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
